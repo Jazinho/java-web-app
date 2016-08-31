f010d7abf9c65bd63c65e6bb216b00f7c532a43a
Jaśko <jan.palucki@gmail.com>
2016-08-31 21:07:11 +0200

    Adding car type enum

---
+++ src/main/java/com/values/CarType.java
@@ -0,0 +1,18 @@
+package com.values;
+
+/**
+ * Enum fo rcar types.
+ */
+
+public enum CarType {
+    SUV,
+    COUPE,
+    HATCHBACK,
+    KOMBI,
+    LIFTBACK,
+    LIMUZYNA,
+    MINIVAN,
+    PICK_UP,
+    SEDAN,
+    VAN
+}
