f7320b5aa7be5697aec9c20ac55cb2e82d79928e
Jaśko <jan.palucki@gmail.com>
2016-09-08 22:27:58 +0200

    Added entity for connecting to database

---
+++ src/main/java/com/entities/CarEntity.java
@@ -0,0 +1,107 @@
+package com.entities;
+
+import com.values.CarType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * CarEntity Entity
+ */
+
+@Entity
+@Table(name = "samochody")
+public class CarEntity {
+
+    @Id
+    private Long id;
+
+    @Column(name = "rok_produkcji", nullable = false)
+    private int price;
+
+    @Column(name = "rok_produkcji", nullable = false)
+    private int productionYear;
+
+    @Column(name = "przejechaneKM", nullable = false)
+    private int passedKms;
+
+    @Column(name = "marka", nullable = false)
+    private String brand;
+
+    @Column(name = "typ_samochodu", nullable = false)
+    private CarType type;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Column(name = "model", nullable = false)
+    private String model;
+
+    protected CarEntity() {
+    }
+
+    public CarEntity(int price, Long id, int productionYear, int passedKms, String marka, CarType type, String model) {
+        this.price = price;
+        this.id = id;
+        this.productionYear = productionYear;
+        this.passedKms = passedKms;
+        this.brand = brand;
+        this.type = type;
+        this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public int getPassedKms() {
+        return passedKms;
+    }
+
+    public void setPassedKms(int passedKms) {
+        this.passedKms = passedKms;
+    }
+
+    public String getMarka() {
+        return brand;
+    }
+
+    public void setMarka(String marka) {
+        this.brand = marka;
+    }
+
+    public CarType getTyp() {
+        return type;
+    }
+
+    public void setTyp(CarType typ) {
+        this.type = typ;
+    }
+}
